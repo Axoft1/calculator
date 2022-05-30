c4812c91f7462554c42581ba93f1e1d11b1e8583
Aleksei Anufriev <axoft10@gmail.com>
2022-05-30 12:51:42 +0700

    img

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-// var text = document.getElementsByClassName('text');
-var textid = document.getElementById('textid')
-var expreshion = '';
 
-function input (num) {
+var textid = document.getElementById('textid')
+let expreshion = '';
+
+function input (num) { // отображает в строке ввода нажимаемые цифры
     expreshion += num;
     textid.value = expreshion;
 }
 
-function ac() {
+function ac() { // обнуляет строку ввода
     expreshion = '';
     textid.value = expreshion;
 }
 
-function equal () {
-    textid.value = eval (expreshion);
-    expreshion = '';
+function equal () { // выводит результат выражения
+    if(textid.value !== ''){
+        textid.value = eval (textid.value);
+    }  
 }
+function beckspace() { // удаляет последний символ
+    let value = document.getElementById("textid").value;
+    textid.value = value.substr(0, value.length - 1);
+    expreshion = textid.value
+}
+
